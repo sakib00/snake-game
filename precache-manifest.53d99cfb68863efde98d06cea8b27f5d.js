fa3aec13c2c3dcbed14b7a5953f2b081ff47f01d
sakib00 <sakib.xc@gmail.com>
2021-10-28 19:03:55 +0600

    Updates

---
+++ precache-manifest.53d99cfb68863efde98d06cea8b27f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2722ddd7296d0e48615f9ef48091365a",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "b0040eb8a367edd89685",
+    "url": "/snake-game/static/css/main.1c974c34.chunk.css"
+  },
+  {
+    "revision": "71467eb90158ce79f159",
+    "url": "/snake-game/static/js/2.d9410911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.d9410911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0040eb8a367edd89685",
+    "url": "/snake-game/static/js/main.b960692c.chunk.js"
+  },
+  {
+    "revision": "18005e22df855f84fc12",
+    "url": "/snake-game/static/js/runtime-main.f9e1bf59.js"
+  }
+]);
